84f30780b1e5f44a8a40cfd2ae3ea301febf32a2
Privatech <99482159+Privatech38@users.noreply.github.com>
2025-08-12 23:09:51 +0200

    Updated platform version and set version to project version to 0.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.1.0"
-    id("org.jetbrains.intellij.platform") version "2.5.0"
+    id("org.jetbrains.intellij.platform") version "2.7.1"
 }
 
 group = "com.github.privatech"
-version = "1.0-SNAPSHOT"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
