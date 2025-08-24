7076920c340cecb08440661447974cb61bff009a
Privatech <99482159+Privatech38@users.noreply.github.com>
2025-08-24 11:51:12 +0200

    Add syntax highlighter

---
+++ src/main/kotlin/com/github/privatech/minimessage/MiniMessageSyntaxHighlighter.kt
@@ -0,0 +1,40 @@
+package com.github.privatech.minimessage
+
+import com.github.privatech.minimessage.psi.MiniMessageTypes
+import com.intellij.lexer.Lexer
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.HighlighterColors
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
+import com.intellij.psi.tree.IElementType
+
+import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey
+import com.intellij.psi.TokenType
+
+class MiniMessageSyntaxHighlighter : SyntaxHighlighterBase() {
+
+    val TAG: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_TAG", DefaultLanguageHighlighterColors.KEYWORD)
+    val STRING: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_STRING", DefaultLanguageHighlighterColors.STRING)
+    val ARGUMENT: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_ARGUMENT", DefaultLanguageHighlighterColors.MARKUP_ATTRIBUTE)
+    val BAD_CHARACTER: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER)
+
+    val BAD_CHAR_KEYS: Array<TextAttributesKey> = arrayOf(BAD_CHARACTER)
+    val TAG_KEYS: Array<TextAttributesKey> = arrayOf(TAG)
+    val STRING_KEYS: Array<TextAttributesKey> = arrayOf(STRING)
+    val ARGUMENT_KEYS: Array<TextAttributesKey> = arrayOf(ARGUMENT)
+    val EMPTY_KEYS: Array<TextAttributesKey> = emptyArray()
+
+    override fun getHighlightingLexer(): Lexer {
+        return MiniMessageLexerAdapter()
+    }
+
+    override fun getTokenHighlights(tokenType: IElementType?): Array<out TextAttributesKey?> {
+        return when (tokenType) {
+            MiniMessageTypes.TAG_NAME -> TAG_KEYS
+            MiniMessageTypes.STRING -> STRING_KEYS
+            MiniMessageTypes.ARGUMENT -> ARGUMENT_KEYS
+            TokenType.BAD_CHARACTER -> BAD_CHAR_KEYS
+            else -> EMPTY_KEYS
+        }
+    }
+}
