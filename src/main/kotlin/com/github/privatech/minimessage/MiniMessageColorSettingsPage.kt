eca73b04169011991e5ad74c8462a0b626add81f
Žiga Slatnar Štagar <99482159+Privatech38@users.noreply.github.com>
2025-08-27 15:17:44 +0200

    Color settings (#6)
    * Update colors and make the vals in a companion object
    
    * Add color settings page
    
    * Update colors for syntax highlighter
    
    * Fix typo
    
    * Update syntax highlighter to use different color for separators, slashes and GT/LT

---
+++ src/main/kotlin/com/github/privatech/minimessage/MiniMessageColorSettingsPage.kt
@@ -0,0 +1,53 @@
+package com.github.privatech.minimessage
+
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.fileTypes.SyntaxHighlighter
+import com.intellij.openapi.options.colors.AttributesDescriptor
+import com.intellij.openapi.options.colors.ColorDescriptor
+import com.intellij.openapi.options.colors.ColorSettingsPage
+import com.intellij.openapi.util.NlsContexts
+import org.jetbrains.annotations.NonNls
+import javax.swing.Icon
+
+class MiniMessageColorSettingsPage : ColorSettingsPage {
+
+    val DESCRIPTORS: Array<AttributesDescriptor> = arrayOf(
+        AttributesDescriptor("Tag", MiniMessageSyntaxHighlighter.TAG),
+        AttributesDescriptor("Tag//Name", MiniMessageSyntaxHighlighter.TAG_NAME),
+        AttributesDescriptor("Tag//Custom name", MiniMessageSyntaxHighlighter.CUSTOM_TAG_NAME),
+        AttributesDescriptor("Tag//String", MiniMessageSyntaxHighlighter.STRING),
+        AttributesDescriptor("Tag//Argument", MiniMessageSyntaxHighlighter.ARGUMENT),
+        AttributesDescriptor("Bad character", MiniMessageSyntaxHighlighter.BAD_CHARACTER)
+    )
+
+    override fun getIcon(): Icon? {
+        return null
+    }
+
+    override fun getHighlighter(): SyntaxHighlighter {
+        return MiniMessageSyntaxHighlighter()
+    }
+
+    override fun getDemoText(): @NonNls String {
+        return """
+            <red>This is red</red>
+            <hover:show_text:'Hello!'>Hover over me!</hover>
+        """.trimIndent()
+    }
+
+    override fun getAdditionalHighlightingTagToDescriptorMap(): Map<String?, TextAttributesKey?>? {
+        return null
+    }
+
+    override fun getAttributeDescriptors(): Array<out AttributesDescriptor?> {
+        return DESCRIPTORS
+    }
+
+    override fun getColorDescriptors(): Array<out ColorDescriptor?> {
+        return ColorDescriptor.EMPTY_ARRAY
+    }
+
+    override fun getDisplayName(): @NlsContexts.ConfigurableName String {
+        return "MiniMessage"
+    }
+}
--- src/main/kotlin/com/github/privatech/minimessage/MiniMessageSyntaxHighlighter.kt
+++ src/main/kotlin/com/github/privatech/minimessage/MiniMessageSyntaxHighlighter.kt
@@ -2,8 +2,8 @@ package com.github.privatech.minimessage
 
 import com.github.privatech.minimessage.psi.MiniMessageTypes
 import com.intellij.lexer.Lexer
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.HighlighterColors
+import com.intellij.openapi.editor.XmlHighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
@@ -12,16 +12,36 @@ import com.intellij.psi.tree.IElementType
 
 class MiniMessageSyntaxHighlighter : SyntaxHighlighterBase() {
 
-    val TAG: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_TAG", DefaultLanguageHighlighterColors.KEYWORD)
-    val STRING: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_STRING", DefaultLanguageHighlighterColors.STRING)
-    val ARGUMENT: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_ARGUMENT", DefaultLanguageHighlighterColors.MARKUP_ATTRIBUTE)
-    val BAD_CHARACTER: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER)
+    companion object {
+        @JvmField
+        val TAG: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_TAG", XmlHighlighterColors.HTML_TAG)
+        @JvmField
+        val TAG_NAME: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_TAG_NAME", XmlHighlighterColors.HTML_TAG_NAME)
+        @JvmField
+        val CUSTOM_TAG_NAME: TextAttributesKey = createTextAttributesKey("MINIMESSAGE_CUSTOM_TAG_NAME", XmlHighlighterColors.HTML_CUSTOM_TAG_NAME)
+        @JvmField
+        val STRING: TextAttributesKey =
+            createTextAttributesKey("MINIMESSAGE_STRING", XmlHighlighterColors.HTML_ATTRIBUTE_VALUE)
+        @JvmField
+        val ARGUMENT: TextAttributesKey =
+            createTextAttributesKey("MINIMESSAGE_ARGUMENT", XmlHighlighterColors.HTML_ATTRIBUTE_NAME)
+        @JvmField
+        val BAD_CHARACTER: TextAttributesKey =
+            createTextAttributesKey("MINIMESSAGE_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER)
 
-    val BAD_CHAR_KEYS: Array<TextAttributesKey> = arrayOf(BAD_CHARACTER)
-    val TAG_KEYS: Array<TextAttributesKey> = arrayOf(TAG)
-    val STRING_KEYS: Array<TextAttributesKey> = arrayOf(STRING)
-    val ARGUMENT_KEYS: Array<TextAttributesKey> = arrayOf(ARGUMENT)
-    val EMPTY_KEYS: Array<TextAttributesKey> = emptyArray()
+        @JvmField
+        val BAD_CHAR_KEYS: Array<TextAttributesKey> = arrayOf(BAD_CHARACTER)
+        @JvmField
+        val TAG_KEYS: Array<TextAttributesKey> = arrayOf(TAG)
+        @JvmField
+        val TAG_NAME_KEYS: Array<TextAttributesKey> = arrayOf(TAG_NAME)
+        @JvmField
+        val STRING_KEYS: Array<TextAttributesKey> = arrayOf(STRING)
+        @JvmField
+        val ARGUMENT_KEYS: Array<TextAttributesKey> = arrayOf(ARGUMENT)
+        @JvmField
+        val EMPTY_KEYS: Array<TextAttributesKey> = emptyArray()
+    }
 
     override fun getHighlightingLexer(): Lexer {
         return MiniMessageLexerAdapter()
@@ -29,7 +49,8 @@ class MiniMessageSyntaxHighlighter : SyntaxHighlighterBase() {
 
     override fun getTokenHighlights(tokenType: IElementType?): Array<out TextAttributesKey?> {
         return when (tokenType) {
-            MiniMessageTypes.TAG_NAME, MiniMessageTypes.SLASH, MiniMessageTypes.COLON -> TAG_KEYS
+            MiniMessageTypes.COLON, MiniMessageTypes.SLASH -> TAG_KEYS
+            MiniMessageTypes.TAG_NAME -> TAG_NAME_KEYS
             MiniMessageTypes.STRING -> STRING_KEYS
             MiniMessageTypes.ARGUMENT -> ARGUMENT_KEYS
             TokenType.BAD_CHARACTER -> BAD_CHAR_KEYS
