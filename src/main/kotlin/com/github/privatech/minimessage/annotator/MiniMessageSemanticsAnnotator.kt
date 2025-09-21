2189d01177c49a771083f0cd2a0f57302e23c524
Privatech <99482159+Privatech38@users.noreply.github.com>
2025-09-21 12:28:24 +0200

    Update legacy code error

--- src/main/kotlin/com/github/privatech/minimessage/annotator/MiniMessageSemanticsAnnotator.kt
+++ src/main/kotlin/com/github/privatech/minimessage/annotator/MiniMessageSemanticsAnnotator.kt
@@ -10,7 +10,8 @@ import com.intellij.psi.PsiElement
 class MiniMessageSemanticsAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
         if (element.node.elementType == MiniMessageTypes.LEGACY_FORMATTING_CODE)
-            holder.newAnnotation(HighlightSeverity.ERROR, "Legacy formatting codes are deprecated")
+            holder.newAnnotation(HighlightSeverity.ERROR, "Legacy formatting codes are deprecated " +
+                    "and not allowed inside MiniMessage strings")
                 .highlightType(ProblemHighlightType.ERROR)
                 .range(element).create()
 
