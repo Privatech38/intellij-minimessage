6c53f5e0831b61a48bb86dd7628bdd7ab11196a5
Privatech <99482159+Privatech38@users.noreply.github.com>
2025-09-24 17:25:44 +0200

    Refactor MiniMessagePsiImplUtil to improve getTextRange return type and clean up toString method

--- src/main/kotlin/com/github/privatech/minimessage/psi/impl/MiniMessagePsiImplUtil.kt
+++ src/main/kotlin/com/github/privatech/minimessage/psi/impl/MiniMessagePsiImplUtil.kt
@@ -1,17 +1,18 @@
 package com.github.privatech.minimessage.psi.impl
 
 import com.github.privatech.minimessage.psi.MiniMessageTagArgument
+import com.intellij.openapi.util.TextRange
 
 class MiniMessagePsiImplUtil {
 
     companion object {
         @JvmStatic
         fun toString(tagArgument: MiniMessageTagArgument): String? {
-            return tagArgument.argument?.text ?: tagArgument.string?.text?.trim('"', '\'');
+            return tagArgument.argument?.text ?: tagArgument.string?.text?.trim('"', '\'')
         }
 
         @JvmStatic
-        fun getTextRange(tagArgument: MiniMessageTagArgument) =
+        fun getTextRange(tagArgument: MiniMessageTagArgument): TextRange =
             tagArgument.argument?.textRange ?: tagArgument.string?.textRange ?: tagArgument.textRange
     }
 
