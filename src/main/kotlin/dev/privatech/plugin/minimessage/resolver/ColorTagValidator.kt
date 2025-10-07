d0a8b26b910c0aab9bcc92a785181513f3d2247a
Privatech <99482159+Privatech38@users.noreply.github.com>
2025-10-07 18:25:03 +0200

    Add ColorTagValidator for MiniMessage color validation

---
+++ src/main/kotlin/dev/privatech/plugin/minimessage/resolver/ColorTagValidator.kt
@@ -0,0 +1,41 @@
+package dev.privatech.plugin.minimessage.resolver
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.psi.PsiElement
+import dev.privatech.plugin.minimessage.psi.MiniMessageTagArgument
+import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.TextColor
+import java.util.*
+
+class ColorTagValidator : TagValidator() {
+    override fun validate(tagName: PsiElement, arguments: LinkedList<MiniMessageTagArgument>, holder: AnnotationHolder) {
+        if (!isColorOrAbbreviation(tagName.text)) {
+            return
+        }
+        val colorArg: MiniMessageTagArgument? = arguments.poll()
+        if (colorArg == null) {
+            holder.newAnnotation(HighlightSeverity.ERROR, "Missing color argument").range(tagName).create()
+            return
+        }
+    }
+
+    override fun has(tagName: String): Boolean {
+        return NamedTextColor.NAMES.value(tagName) != null
+            || isColorOrAbbreviation(tagName)
+            || COLOR_ALIASES.containsKey(tagName)
+            || tagName.matches(Regex("#[0-9a-fA-F]{6}")) // Hex color code
+
+    }
+
+    private fun isColorOrAbbreviation(tagName: String): Boolean {
+        return tagName == "color" || tagName == "colour" || tagName == "c"
+    }
+
+    companion object {
+        private val COLOR_ALIASES: Map<String, TextColor> = mapOf(
+            "dark_grey" to NamedTextColor.DARK_GRAY,
+            "grey" to NamedTextColor.GRAY
+        )
+    }
+}
