165902f4b9b74db3346059ee9599b0d14f19358e
Privatech <99482159+Privatech38@users.noreply.github.com>
2025-10-10 14:10:59 +0200

    Rework getTrimmedArgument to return correctly trimmed and escaped chars

--- src/main/kotlin/dev/privatech/plugin/minimessage/psi/impl/MiniMessagePsiImplUtil.kt
+++ src/main/kotlin/dev/privatech/plugin/minimessage/psi/impl/MiniMessagePsiImplUtil.kt
@@ -2,6 +2,7 @@ package dev.privatech.plugin.minimessage.psi.impl
 
 import com.intellij.openapi.util.TextRange
 import dev.privatech.plugin.minimessage.psi.MiniMessageTagArgument
+import dev.privatech.plugin.minimessage.psi.MiniMessageTypes
 
 class MiniMessagePsiImplUtil {
 
@@ -12,7 +13,15 @@ class MiniMessagePsiImplUtil {
          */
         @JvmStatic
         fun getTrimmedArgument(tagArgument: MiniMessageTagArgument): String {
-            return tagArgument.text.substringAfter(':').trim('"', '\'')
+            return tagArgument.textList.joinToString("") { textElement ->
+                if (textElement.escapedChar != null)
+                    if (textElement.escapedChar!!.text[1] == 'n')
+                        "\n"
+                    else
+                        textElement.escapedChar!!.text[1].toString()
+                else
+                    textElement.text
+            }
         }
 
         @JvmStatic
