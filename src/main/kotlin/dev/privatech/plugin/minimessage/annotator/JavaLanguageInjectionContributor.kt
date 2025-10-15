8a45d1cf243dec825699ce7c49a48fd0ef826a45
Privatech <99482159+Privatech38@users.noreply.github.com>
2025-10-15 16:35:12 +0200

    Attempt to inject into method

--- src/main/kotlin/dev/privatech/plugin/minimessage/annotator/JavaLanguageInjectionContributor.kt
+++ src/main/kotlin/dev/privatech/plugin/minimessage/annotator/JavaLanguageInjectionContributor.kt
@@ -2,16 +2,23 @@ package dev.privatech.plugin.minimessage.annotator
 
 import com.intellij.lang.injection.general.Injection
 import com.intellij.lang.injection.general.LanguageInjectionContributor
-import com.intellij.psi.PsiClass
+import com.intellij.lang.injection.general.SimpleInjection
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiMethodCallExpression
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiReferenceExpression
+import com.intellij.psi.PsiType
+import dev.privatech.plugin.minimessage.MiniMessageLanguage
+import net.kyori.adventure.text.minimessage.MiniMessage
 
 class JavaLanguageInjectionContributor : LanguageInjectionContributor {
     override fun getInjection(element: PsiElement): Injection? {
-        if (element !is PsiMethodCallExpression) return null
-        element.methodExpression.qualifierExpression?.let {
-            if (it.type)
-        }
-        return null
+        if (element !is PsiReferenceExpression) return null
+        val method = element.reference?.resolve() ?: return null
+        if (method !is PsiMethod) return null
+        // Check if method is deserialize and is called on MiniMessage
+        if (!method.name.startsWith("deserialize")) return null
+        val qualifierExpression: PsiType = element.qualifierExpression?.type ?: return null
+        if (qualifierExpression.canonicalText != MiniMessage::class.java.name) return null
+        return SimpleInjection(MiniMessageLanguage.INSTANCE, "", "", null)
     }
 }
