57cd231cda9e8748637ddff330487a18fec64e77
Privatech <99482159+Privatech38@users.noreply.github.com>
2025-10-15 16:59:29 +0200

    Fix injecting into method instead of parameter

--- src/main/kotlin/dev/privatech/plugin/minimessage/annotator/JavaLanguageInjectionContributor.kt
+++ src/main/kotlin/dev/privatech/plugin/minimessage/annotator/JavaLanguageInjectionContributor.kt
@@ -4,7 +4,10 @@ import com.intellij.lang.injection.general.Injection
 import com.intellij.lang.injection.general.LanguageInjectionContributor
 import com.intellij.lang.injection.general.SimpleInjection
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiExpressionList
+import com.intellij.psi.PsiLiteralExpression
 import com.intellij.psi.PsiMethod
+import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.psi.PsiReferenceExpression
 import com.intellij.psi.PsiType
 import dev.privatech.plugin.minimessage.MiniMessageLanguage
@@ -12,12 +15,14 @@ import net.kyori.adventure.text.minimessage.MiniMessage
 
 class JavaLanguageInjectionContributor : LanguageInjectionContributor {
     override fun getInjection(element: PsiElement): Injection? {
-        if (element !is PsiReferenceExpression) return null
-        val method = element.reference?.resolve() ?: return null
-        if (method !is PsiMethod) return null
+        if (element !is PsiLiteralExpression ||
+            element.parent !is PsiExpressionList ||
+            element.parent.parent !is PsiMethodCallExpression) return null
+        val methodReference: PsiMethodCallExpression = element.parent.parent as PsiMethodCallExpression;
+        val method: PsiMethod = methodReference.resolveMethod() ?: return null
         // Check if method is deserialize and is called on MiniMessage
         if (!method.name.startsWith("deserialize")) return null
-        val qualifierExpression: PsiType = element.qualifierExpression?.type ?: return null
+        val qualifierExpression: PsiType = methodReference.methodExpression.qualifierExpression?.type ?: return null
         if (qualifierExpression.canonicalText != MiniMessage::class.java.name) return null
         return SimpleInjection(MiniMessageLanguage.INSTANCE, "", "", null)
     }
