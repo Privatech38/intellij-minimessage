fb0c7924d0c642ad15804f54c1871f79f6f54d1b
Privatech <99482159+Privatech38@users.noreply.github.com>
2025-10-15 17:02:27 +0200

    Cleanup file

--- src/main/kotlin/dev/privatech/plugin/minimessage/annotator/JavaLanguageInjectionContributor.kt
+++ src/main/kotlin/dev/privatech/plugin/minimessage/annotator/JavaLanguageInjectionContributor.kt
@@ -8,7 +8,6 @@ import com.intellij.psi.PsiExpressionList
 import com.intellij.psi.PsiLiteralExpression
 import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiMethodCallExpression
-import com.intellij.psi.PsiReferenceExpression
 import com.intellij.psi.PsiType
 import dev.privatech.plugin.minimessage.MiniMessageLanguage
 import net.kyori.adventure.text.minimessage.MiniMessage
@@ -18,9 +17,9 @@ class JavaLanguageInjectionContributor : LanguageInjectionContributor {
         if (element !is PsiLiteralExpression ||
             element.parent !is PsiExpressionList ||
             element.parent.parent !is PsiMethodCallExpression) return null
-        val methodReference: PsiMethodCallExpression = element.parent.parent as PsiMethodCallExpression;
+        val methodReference: PsiMethodCallExpression = element.parent.parent as PsiMethodCallExpression
         val method: PsiMethod = methodReference.resolveMethod() ?: return null
-        // Check if method is deserialize and is called on MiniMessage
+        // Check if method is deserialization and is called on MiniMessage
         if (!method.name.startsWith("deserialize")) return null
         val qualifierExpression: PsiType = methodReference.methodExpression.qualifierExpression?.type ?: return null
         if (qualifierExpression.canonicalText != MiniMessage::class.java.name) return null
