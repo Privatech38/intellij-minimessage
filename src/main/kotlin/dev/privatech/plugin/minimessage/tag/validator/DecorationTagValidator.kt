0752d2d93ed2682499302cd6a9f14aba6b4662f8
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-03-21 12:22:27 +0100

    Remove unused imports

--- src/main/kotlin/dev/privatech/plugin/minimessage/tag/validator/DecorationTagValidator.kt
+++ src/main/kotlin/dev/privatech/plugin/minimessage/tag/validator/DecorationTagValidator.kt
@@ -3,7 +3,6 @@ package dev.privatech.plugin.minimessage.tag.validator
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.psi.PsiElement
-import org.intellij.markdown.parser.markerblocks.providers.HtmlBlockProvider.Companion.TAG_NAMES
 
 class DecorationTagValidator : TagValidator() {
     override fun validate(
