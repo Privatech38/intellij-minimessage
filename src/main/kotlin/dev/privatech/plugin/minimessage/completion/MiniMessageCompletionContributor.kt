40c7ae136d2c3cd7b0a258e420e41c0924e6d823
Privatech <99482159+Privatech38@users.noreply.github.com>
2026-03-29 14:41:53 +0200

    Add icon and insertion handler

--- src/main/kotlin/dev/privatech/plugin/minimessage/completion/MiniMessageCompletionContributor.kt
+++ src/main/kotlin/dev/privatech/plugin/minimessage/completion/MiniMessageCompletionContributor.kt
@@ -5,26 +5,51 @@ import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionProvider
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.codeInsight.completion.InsertHandler
+import com.intellij.codeInsight.lookup.LookupElement
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.icons.AllIcons
 import com.intellij.patterns.PlatformPatterns
 import com.intellij.util.ProcessingContext
-import dev.privatech.plugin.minimessage.psi.MiniMessageTypes
+import dev.privatech.plugin.minimessage.tag.validator.TagValidator
 
 class MiniMessageCompletionContributor : CompletionContributor() {
 
     init {
         extend(
             CompletionType.BASIC,
-            PlatformPatterns.psiElement(MiniMessageTypes.LT),
+            PlatformPatterns.psiElement(),
             object : CompletionProvider<CompletionParameters>() {
                 override fun addCompletions(
                     parameters: CompletionParameters,
                     context: ProcessingContext,
                     result: CompletionResultSet
                 ) {
-
+                    result.addAllElements(LOOKUP_ELEMENTS)
                 }
             }
         )
     }
 
 }
+
+val INSERT_HANDLER = InsertHandler<LookupElement> { insertionContext, _ ->
+    val document = insertionContext.document
+    val tailOffset = insertionContext.tailOffset
+
+    // Avoid duplicating the closing angle bracket when one already exists.
+    if (tailOffset < document.textLength && document.charsSequence[tailOffset] == '>') {
+        insertionContext.editor.caretModel.moveToOffset(tailOffset + 1)
+        return@InsertHandler
+    }
+
+    document.insertString(tailOffset, ">")
+    insertionContext.editor.caretModel.moveToOffset(tailOffset + 1)
+}
+
+val LOOKUP_ELEMENTS = TagValidator.STANDARD_VALIDATORS.flatMap(TagValidator::tags)
+    .map {
+        LookupElementBuilder.create(it)
+            .withIcon(AllIcons.Nodes.Tag)
+            .withInsertHandler(INSERT_HANDLER)
+    }
